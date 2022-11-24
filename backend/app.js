0b78732d6212fdd75e42628b4e5357aea4dfeac6
anil <rajuanil22@gmail.com>
2022-11-24 09:22:03 +0530

    packagejson changed app js changed procfile added

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+        res.sendFile(path.join(__dirname, "frontend","build", "index.html"));
     })
 }
 
